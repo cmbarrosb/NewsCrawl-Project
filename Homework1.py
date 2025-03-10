04e634fee75e1f457a4ebc5caab773c0f03e1d72
carlos <carlosmbarros@icloud.com>
2025-03-10 11:50:44 -0400

    updated name for project and added auxiliary functions to create padded files and count the lines to verify the correctness of the padding

--- Homework1.py
+++
@@ -1,8 +0,0 @@
-def add_pad(input, out): #automate way to add the padding to files
-    with open(input, 'r') as old, open(out, 'w') as new: #opens a text file and creates a copy as a new text file
-        for line in old:
-            pad_sentence = f"<s> {line.strip()} </s>" #removes whitespace and adds padding
-            new.write(pad_sentence + "\n")
-
-add_pad("train-Spring2025.txt", "CBtrain_padded.txt")
-add_pad("test.txt", "CBtest_padded.txt")
